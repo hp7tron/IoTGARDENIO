8a7163a4853ffdfdcde5ee02a7745a77c1099f25
Umut Erol <43744298+hp7tron@users.noreply.github.com>
2020-07-14 16:46:58 +0300

    Add files via upload

---
+++ FirebaseCekilenVeriler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GardenioWeb.Models
+{
+    public class FirebaseCekilenVeriler
+    {
+
+        public string GelenSicaklik { get; set; }
+        public string GelenNem { get; set; }
+        public string GelenSuSeviyesi { get; set; }
+        public string GelenIsik { get; set; }
+        public string KullanıcıAdı { get; set; }
+        public string Parola { get; set; }
+
+
+
+    }
+}
